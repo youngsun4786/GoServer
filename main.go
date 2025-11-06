52a025adbf383396f793d1d8073a708d4c09827c
Youngsun Yoo <youngsun123q@gmail.com>
2025-11-06 02:07:06 -0700

    add handler to server a file

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	const port = "8080"
+	const root = "/"
+	const filepathRoot = "."
 
 	// Create the multiplexer
-	mux := http.NewServeMux();
+	mux := http.NewServeMux()
+	mux.Handle(root, http.FileServer(http.Dir(filepathRoot)))
 
 	// Create the server with configuration
 	server := &http.Server {
@@ -18,7 +21,7 @@ func main() {
 	}
 
 	// Start the server
-	log.Printf("Serving on port %s\n", port)
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
 	// ListenAndServe returns error
 	log.Fatal(server.ListenAndServe())
 }
