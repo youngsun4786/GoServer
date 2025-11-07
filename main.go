0eebc8fff0973fa842ede334cb50172be1b47b6d
Youngsun Yoo <youngsun123q@gmail.com>
2025-11-06 20:39:40 -0700

    add app to be the default path to serve files

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ func main() {
 	const port = "8080"
 	const root = "/"
 	const filepathRoot = "."
+	const readinessEndpoint = "/healthz"
 
 	// Create the multiplexer
 	mux := http.NewServeMux()
-	mux.Handle(root, http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc(readinessEndpoint, handleReadiness)
 
 	// Create the server with configuration
 	server := &http.Server {
@@ -24,4 +26,10 @@ func main() {
 	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
 	// ListenAndServe returns error
 	log.Fatal(server.ListenAndServe())
+}
+
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
